4b4437ba687491670f2229a067402f23e7552dbf
yib90@staryea.com <yib90@staryea.com>
2019-11-26 11:54:17 +0800

    test push

---
+++ src/main/java/T1.java
@@ -0,0 +1,14 @@
+package main.java;
+
+/**
+ * Package: main.java
+ * Description：
+ * Author: yibowddptwd@163.com
+ * Date:  2019/11/26 11:50
+ * Modified By:
+ */
+public class T1 {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
